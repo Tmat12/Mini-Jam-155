0bf8f02d4a75af0a6602146bcbe3e35c9bc05fc5
Ryan Mathews <mathewsryan2@gmail.com>
2024-03-29 17:08:03 -0500

    Rocks destroy boats. Border stops boats. Universal cordinates system for boats.

--- Scripts/Boat.gd
+++ Scripts/Boat.gd
@@ -6,17 +6,23 @@ class_name Boat
 @export var up_texture : Sprite2D
 @export var down_texture : Sprite2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+
 
 # Number of pixels to move the ship
 var grid_pixels = 48
+var x
+var y
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+#first thing that runs
+func _ready():
+	x = ((position.x - 72)/grid_pixels) + 1
+	y = ((position.y - 72)/grid_pixels) + 1
 
-
+#runs every visual frame
 func _process(delta):
 	if Input.is_action_just_pressed("wind_down"):
 		reset_textures()
@@ -43,10 +49,23 @@ func move_ship():
 	if GameData.wind_dir == "None":
 		pass
 	elif GameData.wind_dir == "Up":
+		if y == 1:
+			return
 		position.y -= 48
 	elif GameData.wind_dir == "Down":
+		if y == 11:
+			return
 		position.y += 48
 	elif GameData.wind_dir == "Left":
+		if x == 1:
+			return
 		position.x -= 48
 	elif GameData.wind_dir == "Right":
+		if x == 22:
+			return
 		position.x += 48
+	x = ((position.x - 72)/grid_pixels) + 1
+	y = ((position.y - 72)/grid_pixels) + 1
+
+		
+		
